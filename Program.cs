c59ab4550a76ed435c5a44f4c3811b30d8d562a2
unknown <hulkaach@mail.ru>
2023-02-26 10:10:17 +0300

    Input words

--- Program.cs
+++ Program.cs
@@ -3,4 +3,31 @@
 // выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись
 // исключительно массивами.
 
-Console.WriteLine("Hello, World!");
+Console.Write("Введи размерность массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[size];
+for (int i = 0; i < array.Length; i++)
+{
+        Console.Write($"Введи {i + 1} слово масcива:");
+        array[i] = Console.ReadLine();
+}
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+        Console.Write($"{array[i]} ");
+        
+}
+Console.Write("\b]"); 
+
+// string FillArray(string[] arr, string n)
+// {
+//     string[] arr = new string[n];
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         Console.Write($"Введи {i + 1} слово масcива:");
+//         array[i] = Console.ReadLine();
+//     }
+//     return arr[];
+// }
+
+// FillArray(array, size);
